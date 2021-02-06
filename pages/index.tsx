91db3ac6474d94fac4dbe48c15a06a08f2ad35f1
NEO <neo.one.mail@gmail.com>
2021-02-06 06:38:15 +0200

    migrate to next && tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h2 className="text-5xl text-green-600">Hello World!!</h2>
+    </div>
+  )
+}
+
+export default Home;
